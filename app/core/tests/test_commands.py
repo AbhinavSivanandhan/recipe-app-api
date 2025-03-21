bd628223a5312cc2f12e4da3bfcf1ba9f65ea328
Abhinav Sivanandhan <abhinav.sivanandhan@gmail.com>
2025-03-21 15:09:12 -0400

    TDD tests for wait_for_db with delay testmethod

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ from django.core.management import call_command #allows django to call command b
 from django.db.utils import OperationalError #another exception thrown by db depending on start up stage of db
 from django.test import SimpleTestCase #for unit test, as we're testing without db
 
-@patch('core.management.commands.wait_for_db.Command.check') #uses command.check provided by BaseCommand that allows to check status of db. patch decorator essentially mocks it
+@patch('core.management.commands.wait_for_db.Command.check') #uses command.check provided by BaseCommand that allows to check status of db. patch decorator essentially mocks it. this makes sure all test functions can access patched_check argument
 class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check): #basically the patch_check object from above 
         """Test waiting for database if database is ready"""
@@ -18,3 +18,12 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(database=['default']) #basically checks it we call the right thing once
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check): #Note on decorators+argument is, inside decorators comes first in order of arguments, outside decorator comes later
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+        self.assertEqual(patched_check.call_count, 6) #2 times to check Psycopg2Error, 3 times to check OperationalError, final time to actually get returned value. so our code should call 6 times, make sure we do that there!
+        patched_check.assert_called_with(database=['default'])
